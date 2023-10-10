e95584691191fb1f8ae96f67b2cb4255057ec545
Josh Hidalgo <hidalgj5@gmatc.matc.edu>
2023-10-10 18:34:08 -0500

    Added enpoiny to get a single post

--- API/Controllers/PostsController.cs
+++ API/Controllers/PostsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Domain;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Persistence;
 
@@ -10,7 +11,7 @@ namespace API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class PostsController
+    public class PostsController: ControllerBase
     {
         private readonly DataContext context;
 
@@ -19,12 +20,32 @@ namespace API.Controllers
             this.context = context;
         }
 
-        //GET api/posts
+        /// <summary>
+        /// GET api/post
+        /// </summary>
+        /// <returns>A list of posts</returns>
         [HttpGet (Name = "GetPosts")]
 
         public ActionResult<List<Post>> Get()
         {
             return this.context.Posts.ToList();
+        }
+
+        /// <summary>
+        /// GET api/post/[id]
+        /// </summary>
+        /// <param name="id">Post id</param>
+        /// <returns>A single post</returns>
+        [HttpGet("{id}", Name = "GetById")]
+        public ActionResult<Post> GetById(Guid id)
+        {
+            var post = this.context.Posts.Find(id);
+            if(post is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(post);
         }
     }
 }
